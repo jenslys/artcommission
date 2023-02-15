4c4d4ba160409b0991f5b06c44a4b8ba1b0f7250
Jens <69081683+jenslys@users.noreply.github.com>
2023-02-15 02:14:41 +0100

    add more visible home page

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,3 +1,11 @@
+import Typography from '@mui/material/Typography';
 export default function Form() {
-  return <div>Form</div>;
+  return (
+    <div>
+      <Typography variant='h1' component='h2'>
+        Form Page
+      </Typography>
+      ;
+    </div>
+  );
 }
