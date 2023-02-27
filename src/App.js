707c95c8d91cfff6363781168fe17c2b9c089421
Jens <69081683+jenslys@users.noreply.github.com>
2023-02-27 18:40:37 +0100

    add authcontext

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
 
 // Page components
 import Form from './pages/Form';
 import Login from './pages/Login';
 
 function App() {
+  const { user, authIsReady } = useAuthContext();
   return (
     <div className='App'>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Form />} />
-          <Route path='/Login' element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Form />} />
+            <Route
+              path='/Login'
+              element={user ? <Navigate to='/requests' replace={true} /> : <Login />}
+            />
+          </Routes>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
