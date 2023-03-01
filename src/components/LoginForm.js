2f662a6568dbe2d168745e862c519e48f5bfdafd
Jens <69081683+jenslys@users.noreply.github.com>
2023-03-01 14:38:20 +0100

    Add error notification to the login screen

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,11 +5,11 @@ import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import Alert from '@mui/material/Alert';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import { useState } from 'react';
@@ -75,7 +75,11 @@ export default function LoginForm() {
             <Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
               Sign In
             </Button>
-            <Grid container>{error && <p>{error}</p>}</Grid>
+            {error && (
+              <Alert severity='error' variant='filled'>
+                {error}
+              </Alert>
+            )}
           </Box>
         </Box>
       </Container>
