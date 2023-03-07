104ad981ee8527d9cb66fd28d5ce9fda1b7fabdc
Jens <69081683+jenslys@users.noreply.github.com>
2023-03-07 23:16:31 +0100

    remove remeber me checkbox to login site

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,8 +3,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
@@ -68,12 +66,11 @@ export default function LoginForm() {
               autoComplete='current-password'
               onChange={(e) => setPassword(e.target.value)}
             />
-            <FormControlLabel
-              control={<Checkbox value='remember' color='primary' />}
-              label='Remember me'
-            />
             <Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
               Sign In
+            </Button>
+            <Button onClick={() => window.location.replace('https://artbymuland.no')}>
+              Go back
             </Button>
             {error && (
               <Alert severity='error' variant='filled'>
